8c3498707b2ca84a6a65ee8fc432f99cb67ddfd3
salEZY <bobicaleks@yahoo.com>
2021-10-28 16:11:19 +0200

    add .env

--- config/default.ts
+++ config/default.ts
@@ -1,6 +1,7 @@
+require('dotenv').config()
 export default {
   port: 8080,
   dbUri:
-    "mongodb+srv://salezy:salezy@test.e84x8.mongodb.net/test?retryWrites=true&w=majority",
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@test.e84x8.mongodb.net/test?retryWrites=true&w=majority`,
   salt: 10,
 };
